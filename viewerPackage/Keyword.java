6c24816c884f9a634b004d79b897864d3fb59948
eunjin0817 <gej48443@gmail.com>
2019-06-10 17:36:16 +0900

    ClassViewer

--- viewerPackage/Keyword.java
+++
@@ -1,8 +0,0 @@
-package viewerPackage;
-
-public class Keyword {
-    public static final String CLASS = "class";
-    public static final String[] TYPE = {"void","bool","int"};
-    public static final String[] CONTROLSTATEMENT = {"if","else"};
-    public static final String[] ACCESS = {"public","private"};
-}
--- viewerPackage/Test.java
+++
@@ -1,7 +0,0 @@
-package viewerPackage;
-
-public class Test {
-    public static void main(String[] args){
-        GuiMenu menu = new GuiMenu();
-    }
-}
